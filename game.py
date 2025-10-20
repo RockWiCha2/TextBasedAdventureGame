f801ea141a3f946b9cc61d3f9b897aa96e6c0983
James Harvey <harveyj16@cardiff.ac.uk>
2025-10-20 03:05:49 +0100

    Small change to imports

--- game.py
+++ game.py
@@ -2,12 +2,14 @@
 # Variables must be in camelCase 
 # Functions must be in snake_case
 
-from gui import *
+import gui
 
 
 # Main game loop
 def main():
+    gui.start()
     while True:
+        gui.gui_print("Welcome to the adventure")
         # Call functions for game logic here
         pass  # Placeholder to avoid syntax error
 
