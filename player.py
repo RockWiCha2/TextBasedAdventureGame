883d4b39923aa07d95bf50981e42a4cd55673b6f
Tom Cox <coxtj3@cardiff.ac.uk>
2025-10-27 16:37:29 +0000

    toms shit

--- player.py
+++ player.py
@@ -4,37 +4,33 @@ peasant = {
     "health": 100,
     "damageMult": 1,
     "maxWeight": 2.5,
-    "description": "A simple villager with no training. Weak, but full of determination."
-    
-
+    "mana": 100
     }
 barbarian = {
     "health": 250,
     "damageMult": 1,
     "maxWeight": 8,
-    "description": "A savage warrior with immense strength and stamina."
-
+    "mana": 100
     }
 archer = {
     "health": 125,
     "damageMult": 1.75,
-    "maxWeight": 5,
-    "description": "A skilled hunter who strikes swiftly from a distance."
+    "maxWeight": 5
 
     }
 mage = {
     "health": 150,
     "damageMult": 1,
     "maxWeight": 3.5,
-    "mana": 100,
-    "description": "A wise spellcaster who uses magic to defeat enemies."
+    "mana": 100
     }
 #defines all classes and their different perks/drawbacks (more to be added)
 
 playerClass = peasant
 #sets the players starting class as peasant
 
-inventory = [item_archer_longbow, item_fighter_longsword, item_shield_wood, item_armor_leather_set, item_potion_mana_small, item_potion_health_medium]
+inventory = []
 #sets starting inventory
 
-currentRoom = rooms["sealed_gate"]
+currentRoom = rooms["kidnapped_cell"]
+currentWeapon = item_fighter_longsword
